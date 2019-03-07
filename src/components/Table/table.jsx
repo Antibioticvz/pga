d5a562ce0339ccbde115ac7d7effd53fe4033dbd
antibioticvz@gmail.com <antibioticvz@gmail.com>
2019-03-07 19:51:46 +0400

    added sorted rows

--- src/components/Table/table.jsx
+++ src/components/Table/table.jsx
@@ -1,16 +1,64 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import Paper from '@material-ui/core/Paper'
 
-const Table = () => {
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 700,
+  },
+})
+
+
+const rows = [
+  { firstName: 'Alex', lastName: 'Abrams', score: 99 },
+  { firstName: 'Junge', lastName: 'AAJohn', score: 99 },
+  { firstName: 'Vera', lastName: 'Rob', score: 77 },
+  { firstName: 'Geary', lastName: 'Alice', score: 77 },
+]
+
+function SimpleTable(props) {
+  const { classes } = props
+
+  const sortedData = [...rows].sort((a, b) => (
+    (a.score < b.score) ? 1 : (a.score === b.score) ? ((a.lastName > b.lastName) ? 1 : -1) : -1))
+
   return (
-    <div>
-      Table
-    </div>
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell align="center">Name</TableCell>
+            <TableCell align="center">Score </TableCell>
+            <TableCell align="center">Remove</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {sortedData.map((row, i) => (
+            <TableRow key={i}>
+              <TableCell align="center">{`${row.firstName}, ${row.lastName}`}</TableCell>
+              <TableCell align="center">{row.score}</TableCell>
+              <TableCell align="center">Delete</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Paper>
   )
 }
 
-Table.propTypes = {
-
+SimpleTable.propTypes = {
+  // classes: PropTypes.object.isRequired,
 }
 
-export default Table
+export default withStyles(styles)(SimpleTable)
