9620a18c6d5b18971275fc9fc8f4ff471e62b113
antibioticvz@gmail.com <antibioticvz@gmail.com>
2019-03-07 21:35:46 +0400

    changed styles to hooks

--- src/components/Table/table.jsx
+++ src/components/Table/table.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-nested-ternary */
 import React from 'react'
 import PropTypes from 'prop-types'
-import { withStyles } from '@material-ui/core/styles'
+import { makeStyles } from '@material-ui/styles'
 import Table from '@material-ui/core/Table'
 import TableBody from '@material-ui/core/TableBody'
 import TableCell from '@material-ui/core/TableCell'
@@ -10,20 +10,20 @@ import TableRow from '@material-ui/core/TableRow'
 import Paper from '@material-ui/core/Paper'
 import Button from '@material-ui/core/Button'
 
-const styles = theme => ({
+const tableStyles = makeStyles(() => ({
   root: {
     width: '100%',
-    marginTop: theme.spacing.unit * 3,
     overflowX: 'auto',
   },
   table: {
     minWidth: 700,
   },
-})
+}))
 
 
 const SimpleTable = (props) => {
-  const { classes, delPlayer, rows } = props
+  const { delPlayer, rows } = props
+  const classes = tableStyles()
 
   const sortedData = [...rows].sort((a, b) => (
     (a.score < b.score) ? 1 : (a.score === b.score) ? ((a.lastName > b.lastName) ? 1 : -1) : -1))
@@ -39,8 +39,8 @@ const SimpleTable = (props) => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {sortedData.map((row, i) => (
-            <TableRow key={i}>
+          {sortedData.map(row => (
+            <TableRow key={row.lastName}>
               <TableCell align="center">{`${row.firstName}, ${row.lastName}`}</TableCell>
               <TableCell align="center">{row.score}</TableCell>
               <TableCell align="center">
@@ -57,8 +57,6 @@ const SimpleTable = (props) => {
 }
 
 SimpleTable.propTypes = {
-  // eslint-disable-next-line react/forbid-prop-types
-  classes: PropTypes.object.isRequired,
   rows: PropTypes.arrayOf(PropTypes.shape({
     firstName: PropTypes.string.isRequired,
     lastName: PropTypes.string.isRequired,
@@ -67,4 +65,4 @@ SimpleTable.propTypes = {
   delPlayer: PropTypes.func.isRequired,
 }
 
-export default withStyles(styles)(SimpleTable)
+export default SimpleTable
