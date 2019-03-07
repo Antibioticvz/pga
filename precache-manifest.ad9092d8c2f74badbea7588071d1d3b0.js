b86c1a6dfef5005537a7e35caa288da15918837b
antibioticvz@gmail.com <antibioticvz@gmail.com>
2019-03-07 21:42:44 +0400

    Updates

---
+++ precache-manifest.ad9092d8c2f74badbea7588071d1d3b0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "ece21ca0b293fe8a581c",
+    "url": "/pga/static/js/runtime~main.1f0d02f6.js"
+  },
+  {
+    "revision": "4bb61faa34218f5474ff",
+    "url": "/pga/static/js/main.c35a4c32.chunk.js"
+  },
+  {
+    "revision": "66b77130fe2ee284bf33",
+    "url": "/pga/static/js/2.f9d080ee.chunk.js"
+  },
+  {
+    "revision": "f048befc411452540d78d809f4c92d19",
+    "url": "/pga/index.html"
+  }
+];
